bb61726c7bd3fce916e38a8c604877cf32f732ea
Daniel Londoño <daniellondono17@gmail.com>
2023-03-31 18:49:52 -0500

    Fix JSON Object build

--- managers/clobby.js
+++ managers/clobby.js
@@ -18,7 +18,7 @@ const lobbyingManager = {
                     const bill = $('div.bill-card-front-header').find('a');
                     let billName = [];
                     let billUrl = []
-                    bill.each((index, element) => {
+                    bill.slice(0, 60).each((index, element) => {
                         billUrl.push($(element).attr('href'));
                         billName.push($(element).text());
                     });
@@ -29,7 +29,7 @@ const lobbyingManager = {
 
                     const billDescription = $('div.bill-card-front-inner-left').find('h6');
                     let billDescriptions = [];
-                    billDescription.each((index, element) => {
+                    billDescription.slice(0, 60).each((index, element) => {
                         billDescriptions.push($(element).text());
                     });
 
@@ -42,37 +42,78 @@ const lobbyingManager = {
                     let arrBillLatestUpdates = [];
                     let arrDateLatestUpdates = [];
 
-                    billLatestUpdate.each((index, element) => {
+                    billLatestUpdate.slice(0, 60).each((index, element) => {
                         arrBillLatestUpdates.push($(element).text());
                     });
-                    dateLatestUpdate.each((index, element) => {
+                    dateLatestUpdate.slice(0, 60).each((index, element) => {
                         arrDateLatestUpdates.push($(element).text());
                     });
 
                     /*
                      *   Lobbying Companies
                      */
+
                     const billLobbyingCompanies = $('div.bill-card-back-lobbying-inner');
                     let arrLobbists = [];
-                    billLobbyingCompanies.each((index, element)=>{
+                    billLobbyingCompanies.slice(0, 60).each((index, element) => {
                         let ptCompanies = [];
                         const lobbists = $(element).find('a')
-                        lobbists.each((index, ticker)=>{
+                        lobbists.each((index, ticker) => {
                             ptCompanies.push($(ticker).text());
                         });
                         arrLobbists.push(ptCompanies);
                     });
 
-                    for(let i = 0; i < 120; i++){
-                        console.log('Bill')
-                        console.log(billName[i]);
-                        console.log(billUrl[i]);
-                        console.log('-------')
-                    }
+                    /*
+                     *   Relevant Trades
+                     */
+
+                    const relevantTrades = $('div.bill-card-back-trades-inner');
+                    const arrRelevantTrades = [];
+                    const limit = 60;
+                    relevantTrades.slice(0, limit).each((index, element) => {
+
+                        let relevantTrade = {};
+
+                        let arrPoliticians = [];
+                        let arrStocks = [];
+                        let arrTrades = [];
+                        let arrDates = [];
+
+                        // Politician
+                        const names = $(element).find('div.name-data').find('span');
+                        names.each((index, name)=> {
+                            arrPoliticians.push($(name).text()); 
+                        });
+
+                        // Stock
+                        const stocks = $(element).find('div.stock-data').find('span');
+                        stocks.each((index, stock)=>{
+                            arrStocks.push($(stock).text());
+                        });
+
+                        // Trade
+                        const tradeTypes = $(element).find('div.trade-data').find('span');
+                        tradeTypes.each((index, tt)=>{
+                            arrTrades.push($(tt).text());
+                        });
+
+                        // Date
+                        const dates = $(element).find('div.date-data').find('span');
+                        dates.each((index, date)=> {
+                            arrDates.push($(date).text());
+                        });
+
+                        console.log(arrPoliticians.length);
+                        arrPoliticians.forEach((e)=>{console.log(e)});
+                        console.log('------ Trade break ------');
 
 
 
 
+                    });
+
+                    
 
                 }
             });
